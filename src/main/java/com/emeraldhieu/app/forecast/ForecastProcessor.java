8a06e27610e8bc79d2219d4a23cebaac633e3814
Hieu <emerald.hieu@gmail.com>
2022-11-08 00:50:03 +0700

    Reanme vars a bit; extract share var into TIMESTAMP_OF_DAY

--- src/main/java/com/emeraldhieu/app/forecast/ForecastProcessor.java
+++ src/main/java/com/emeraldhieu/app/forecast/ForecastProcessor.java
@@ -15,16 +15,15 @@ public class ForecastProcessor {
 
     private final Clock clock;
     private final ForecastTimeFormatter forecastTimeFormatter;
+    private static final int TIMESTAMP_OF_DAY = 8; // There are 8 three-hour timestamps a day.
 
     /**
      * Get remaining timestamps of today plus 8 timestamps of the next day.
      * Since the external API doesn't support offset, we have to include remaining timestamps.
      */
     public int getTimestampCount() {
-        int remainingParts = getRemainingTimestampsOfToday();
-        int partsOfDay = 8; // 3-hour 8 parts a day.
-        int count = remainingParts + partsOfDay;
-        return count;
+        int remainingTimestampsOfToday = getRemainingTimestampsOfToday();
+        return remainingTimestampsOfToday + TIMESTAMP_OF_DAY;
     }
 
     /**
@@ -34,9 +33,7 @@ public class ForecastProcessor {
     private int getRemainingTimestampsOfToday() {
         int currentHour = clock.getCurrentTime().getHour(); // e.g. 18
         int remainingHour = 24 - currentHour;
-        int partsOfDay = 8; // 3-hour 8 parts a day.
-        int remainingParts = (int) Math.ceil((double) remainingHour * partsOfDay / 24) - 1;
-        return remainingParts;
+        return (int) Math.ceil((double) remainingHour * TIMESTAMP_OF_DAY / 24) - 1;
     }
 
     public boolean isWarm(double averageTemperature, double minTemperature) {
